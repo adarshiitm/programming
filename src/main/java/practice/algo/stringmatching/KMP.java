62e4a37a32033d7565947e11a4ce6164549ae5d8
Adarsh Sharma <adarsh.sharma@flipkart.com>
2018-07-10 01:01:54 +0530

    IB

--- src/main/java/practice/algo/stringmatching/KMP.java
+++ src/main/java/practice/algo/stringmatching/KMP.java
@@ -10,7 +10,7 @@ import java.util.List;
  * Created by adarsh.sharma on 16/12/17.
  */
 public class KMP {
-    public static Collection<Integer> getMatches(String s, String pattern) {
+    public Collection<Integer> getMatches(String s, String pattern) {
         List<Integer> matches = new ArrayList<>();
         if (s == null || pattern == null || s.length() < pattern.length()) {
             return matches;
@@ -38,7 +38,7 @@ public class KMP {
         return matches;
     }
 
-    private static Integer[] computePrefixFunction(String pattern) {
+    public Integer[] computePrefixFunction(String pattern) {
         int length = pattern.length();
         Integer[] lps = new Integer[length];
 
@@ -62,6 +62,6 @@ public class KMP {
     public static void main(String[] args) {
         String s = "ABABDABACDABABCABAB";
         String p = "ABABCABAB";
-        System.out.println(StringUtils.join(getMatches(s, p), ','));
+        System.out.println(StringUtils.join(new KMP().getMatches(s, p), ','));
     }
 }
