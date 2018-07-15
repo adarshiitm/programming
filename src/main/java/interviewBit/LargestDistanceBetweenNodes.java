cf1eca4c6277172b13fe8c06b65cc61e968094f5
Adarsh Sharma <adarsh.sharma@flipkart.com>
2018-07-15 23:03:50 +0530

    algos

--- src/main/java/interviewBit/LargestDistanceBetweenNodes.java
+++ src/main/java/interviewBit/LargestDistanceBetweenNodes.java
@@ -48,9 +48,29 @@ public class LargestDistanceBetweenNodes {
         return max;
     }
 
+    public int solve2(List<Integer> A) {
+        List<int[]> arr = new ArrayList<>();
+        for (int i = 0; i < A.size(); ++i) {
+            arr.add(new int[2]);
+        }
+        int maxDistance = 0;
+        for (int i = A.size() - 1; i > 0; --i) {
+            int element = A.get(i);
+            int[] parent = arr.get(element);
+            int currentLength = arr.get(i)[0] + 1;
+
+            parent[1] = Math.max(parent[1], currentLength + parent[0]);
+            parent[0] = Math.max(parent[0], currentLength);
+            maxDistance = Math.max(maxDistance, parent[1]);
+        }
+        return maxDistance == 0 ? 0 : maxDistance;
+    }
+
     public static void main(String[] args) {
-//        System.out.println(new LargestDistanceBetweenNodes().solve(Arrays.asList(-1, 2, 0, 0, 3, 3)));
-        System.out.println(new LargestDistanceBetweenNodes().solve(Arrays.asList(-1)));
+//        System.out.println(new LargestDistanceBetweenNodes().solve(Arrays.asList(-1)));
+//        System.out.println(new LargestDistanceBetweenNodes().solve2(Arrays.asList(-1)));
+        System.out.println(new LargestDistanceBetweenNodes().solve(Arrays.asList(-1, 2, 0, 0, 3, 3)));
+        System.out.println(new LargestDistanceBetweenNodes().solve2(Arrays.asList(-1, 2, 0, 0, 3, 3)));
 //        System.out.println(new LargestDistanceBetweenNodes().solve(Arrays.asList(-1, 0, 0, 0, 3)));
     }
 }
---
+++ src/main/java/practice/algo/graphs/DisjointSet.java
@@ -0,0 +1,61 @@
+package practice.algo.graphs;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by adarsh.sharma on 15/07/18.
+ */
+public class DisjointSet {
+    Map<Integer, Node> mp = new HashMap<>();
+
+    class Node {
+        int value;
+        int rank;
+        Node parent;
+
+        public Node(int value, int rank) {
+            this.value = value;
+            this.rank = rank;
+        }
+    }
+
+    public void makeSet(int value) {
+        Node n = new Node(value, 0);
+        n.parent = n;
+        mp.put(value, n);
+    }
+
+    public int findSet(int value) {
+        return findSet(mp.get(value)).value;
+    }
+
+
+    private Node findSet(Node node) {
+        Node parent = node.parent;
+        if (parent == node) {
+            return parent;
+        }
+        node.parent = findSet(node.parent);
+        return node.parent;
+    }
+
+    public void union(int value1, int value2) {
+        Node node1 = mp.get(value1);
+        Node node2 = mp.get(value2);
+
+        Node parent1 = findSet(node1);
+        Node parent2 = findSet(node2);
+
+        if (parent1.value == parent2.value) {
+            return;
+        }
+
+        if(parent1.rank >= parent2.rank) {
+            parent1.rank = (parent1.rank == parent2.rank)? parent1.rank + 1: parent1.rank;
+            parent2.parent = parent1;
+        } else {
+            parent1.parent = parent2;
+        }
+    }
+}
