2648b0c6e8f0e49b9f6ed21c2f83fb85aa7a3803
Adarsh Sharma <adarsh.sharma@flipkart.com>
2018-08-11 23:22:53 +0530

    Simple

--- src/main/java/interviewBit/AddBinaryStrings.java
+++ src/main/java/interviewBit/AddBinaryStrings.java
@@ -5,22 +5,24 @@ package interviewBit;
  */
 public class AddBinaryStrings {
     public String addBinary(String A, String B) {
+        if (A == null || B == null) {
+            return A == null ? B : A;
+        }
+
         int i = A.length() - 1;
         int j = B.length() - 1;
-        StringBuilder res = new StringBuilder();
+        StringBuilder res = new StringBuilder(Math.max(A.length(), B.length()) + 1);
         int s = 0;
         int base = 2;
         while (i >= 0 || j >= 0) {
             if (i >= 0) {
-                s += A.charAt(i) - '0';
+                s += A.charAt(i--) - '0';
             }
             if (j >= 0) {
-                s += B.charAt(j) - '0';
+                s += B.charAt(j--) - '0';
             }
             res.append(s % base);
             s = s / base;
-            i--;
-            j--;
         }
 
         if (s > 0) {
@@ -32,6 +34,6 @@ public class AddBinaryStrings {
     }
 
     public static void main(String[] args) {
-        System.out.println(new AddBinaryStrings().addBinary("01101000", "11100110"));
+        System.out.println(new AddBinaryStrings().addBinary("1101000", "11100110"));
     }
 }
