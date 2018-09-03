d9ea4cb1ba9a4922836dc211ebf6453f0d864514
Adarsh Sharma <adarsh.sharma@flipkart.com>
2018-09-03 16:57:51 +0530

    code

--- src/main/java/practice/algo/misc/Test.java
+++ src/main/java/practice/algo/misc/Test.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
  * Created by adarsh.sharma on 11/06/18.
  */
 public class Test {
+
     public static void main(String[] args) {
         int[] a = {1,3,4,7,9};
         System.out.println(Arrays.binarySearch(a, 0, a.length, 4));
