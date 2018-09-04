a839dd871357e1d3bd362f0e0212d20f2f5c51a1
Adarsh Sharma <adarsh.sharma@flipkart.com>
2018-09-04 13:52:43 +0530

    Done

--- src/main/java/practice/algo/misc/Test.java
+++ src/main/java/practice/algo/misc/Test.java
@@ -1,15 +1,11 @@
 package practice.algo.misc;
 
-import java.util.Arrays;
-
 /**
  * Created by adarsh.sharma on 11/06/18.
  */
 public class Test {
-
     public static void main(String[] args) {
-        int[] a = {1,3,4,7,9};
-        System.out.println(Arrays.binarySearch(a, 0, a.length, 4));
-        System.out.println(Arrays.binarySearch(a, 0, a.length, 8));
+        String a = "113112";
+        System.out.println(a.startsWith("112"));
     }
 }
