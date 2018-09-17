00a32f83c23862f95713252e62040960a78f8dfb
Adarsh Sharma <adarsh.sharma@flipkart.com>
2018-09-17 10:10:15 +0530

    Added code

--- src/main/java/practice/algo/misc/Test.java
+++ src/main/java/practice/algo/misc/Test.java
@@ -1,17 +1,35 @@
 package practice.algo.misc;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 /**
  * Created by adarsh.sharma on 11/06/18.
  */
 public class Test {
+    class P {
+        int x;
+        int y;
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            P p = (P) o;
+            return x == p.x &&
+                    y == p.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+    }
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        list.add(10);
-        list.add(8);
-        list.add(6);
-        list.add(2);
-        System.out.println(Collections.binarySearch(list, 11, Collections.reverseOrder()));
+        int[] ints = IntStream.range(0, 0).toArray();
+        Arrays.stream(ints).forEach(System.out::println);
     }
 }
+
+
